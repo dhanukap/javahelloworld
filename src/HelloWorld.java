3c47ff8407bfcc9dccaa1ab2713baa506915c872
root <Dhanuka Perera>
2016-05-17 15:20:56 +1000

    modified HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main (String [] args)
 	{
-		System.out.println("hello world");
+		System.out.println("hello world. 2nd time");
 		System.exit(0);
 	}
 }
